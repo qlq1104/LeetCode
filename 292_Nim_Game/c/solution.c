c0e99a4c1820f3e45d1538c6f77ab52af81a5476
qlq1104 <961273509@qq.com>
2016-04-13 22:17:26 +0800

    change to google code style

--- 292_Nim_Game/c/solution.c
+++ 292_Nim_Game/c/solution.c
@@ -1,19 +1,16 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool canWinNim(int n) 
-{
-	return (n % 4 == 0);
+bool canWinNim(int n) {
+  return (n % 4 == 0);
 }
 
-int main()
-{
- 	int i, cases[] = {0, 1, 2, 4, 8, 9, 112, 439, 23234};
+int main() {
+  int i, cases[] = {0, 1, 2, 4, 8, 9, 112, 439, 23234};
  
- 	for (i=0; i<sizeof(cases)/sizeof(int); i++) 
- 	{
-		printf("[%5d]:%d\n", cases[i], canWinNim(cases[i]));
-	}
-	
-	return 0;
+  for (i=0; i<sizeof(cases)/sizeof(int); i++) {
+    printf("[%5d]:%d\n", cases[i], canWinNim(cases[i]));
+  }
+
+  return 0;
 }
--- 292_Nim_Game/cpp/solution.cpp
+++ 292_Nim_Game/cpp/solution.cpp
@@ -3,21 +3,18 @@ using namespace std;
 
 class Solution {
 public:
-    bool canWinNim(int n) 
-    {
-        return !(n%4 == 0);
-    }
+  bool canWinNim(int n)  {
+    return !(n%4 == 0);
+  }
 };
 
-int main()
-{
-	int testCase[] = {0, 1, 2, 4, 8, 9, 112, 439, 23234};
-	Solution s;
+int main() {
+  int testCase[] = {0, 1, 2, 4, 8, 9, 112, 439, 23234};
+  Solution s;
 
-	for (int i=0; i<9; i++) 
-	{
-		cout << "[" << testCase[i] << "]: " << s.canWinNim(testCase[i]) << endl;
-	}
-
-	return 0;
+  for (int i=0; i<9; i++) {
+    cout << "[" << testCase[i] << "]: " << s.canWinNim(testCase[i]) << endl;
+  }
+  
+  return 0;
 }
